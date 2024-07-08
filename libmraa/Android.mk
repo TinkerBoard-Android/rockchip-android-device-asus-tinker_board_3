01d2d4d219e50f1ca46359660dc46187666aeb22
locker_kao <locker_kao@asus.com>
2024-07-08 16:40:42 +0800

    14Pin: Mraa: Fix 3N android14 PWM start from pwmchip1
    - Temporarily separate mraa library between 3 and 3N
    
    Change-Id: I54487ea3ae7d884177a36f889cc9dc875a656a85
    Signed-off-by: locker_kao <locker_kao@asus.com>

--- libmraa/Android.mk
+++ libmraa/Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(PRODUCT_NAME), $(filter $(PRODUCT_NAME),Tinker_Board_3 Tinker_Board_3N Sanden_VM Sanden_CM))
+ifeq ($(PRODUCT_NAME), $(filter $(PRODUCT_NAME), Tinker_Board_3N Sanden_VM Sanden_CM))
 
 LOCAL_PATH := $(call my-dir)
 
@@ -12,7 +12,7 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_SHARED_LIBRARIES := libc libdl libm
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_SRC_FILES_64 := lib64/libmraa.so
-LOCAL_PREBUILT_JNI_LIBS := lib64/libc++_shared.so
+LOCAL_PREBUILT_JNI_LIBS := 3n/lib64/libc++_shared.so
 LOCAL_MODULE_TARGET_ARCH:= arm64
 include $(BUILD_PREBUILT)
 
@@ -25,7 +25,39 @@ LOCAL_MULTILIB := 64
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_SHARED_LIBRARIES := libc libdl libm libmraa
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES_64 := lib64/libmraajava.so
+LOCAL_SRC_FILES_64 := 3n/lib64/libmraajava.so
+LOCAL_MODULE_TARGET_ARCH:= arm64
+include $(BUILD_PREBUILT)
+endif
+
+ifeq ($(PRODUCT_NAME), $(filter $(PRODUCT_NAME),Tinker_Board_3))
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE := libmraa
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 64
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SHARED_LIBRARIES := libc libdl libm
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES_64 := 3/lib64/libmraa.so
+LOCAL_PREBUILT_JNI_LIBS := 3/lib64/libc++_shared.so
+LOCAL_MODULE_TARGET_ARCH:= arm64
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_MODULE := libmraajava
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 64
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SHARED_LIBRARIES := libc libdl libm libmraa
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES_64 := 3/lib64/libmraajava.so
 LOCAL_MODULE_TARGET_ARCH:= arm64
 include $(BUILD_PREBUILT)
 endif
